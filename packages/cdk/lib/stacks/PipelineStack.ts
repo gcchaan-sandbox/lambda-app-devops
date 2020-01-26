880be1f3947977030e9f2ec5696f7f7d554bc05c
gcchaan <gcchaan@users.noreply.github.com>
2020-01-27 06:10:22 +0900

    install yarn to ci

--- packages/cdk/lib/stacks/PipelineStack.ts
+++ packages/cdk/lib/stacks/PipelineStack.ts
@@ -22,6 +22,16 @@ export class PipelineStack extends Stack {
         version: '0.2',
         phases: {
           install: {
+            commands: [
+              '# install yarn',
+              'sudo apt-get update && sudo apt-get install apt-transport-https',
+              'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -',
+              'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list',
+              'sudo apt-get update && sudo apt-get install yarn',
+              'yarn --version',
+            ]
+          },
+          pre_build: {
             commands: 'yarn install',
           },
           build: {
